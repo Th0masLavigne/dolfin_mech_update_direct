fd17027723b090d0be28065a33c16565152cf751
Th0masLavigne <91893320+Th0masLavigne@users.noreply.github.com>
2026-02-15 16:17:20 +0000

    Deploying to gh-pages from @ Th0masLavigne/dolfin_mech_devel_lavigne@bdd86819aed809d4944fe6004e4e9247479de1d1 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.0.post1+g4458e6a83',
+    VERSION: '0.0.0.post1+gbdd86819a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
